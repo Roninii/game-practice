8f992dd7fcaf68be3c04a100975d85f79c934d5c
= <=>
2020-09-10 21:06:44 -0500

    add license and project bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello typescript')
+const canvas: HTMLCanvasElement = document.getElementById(
+  'game-screen'
+) as HTMLCanvasElement
+
+const ctx = canvas.getContext('2d')
+
+if (ctx) {
+  ctx.fillStyle = '#f00'
+  ctx.fillRect(20, 20, 100, 100)
+}
